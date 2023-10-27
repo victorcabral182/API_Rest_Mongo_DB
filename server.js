46db28d14474938a9c81662a3e7578f1ae30779d
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-10-26 21:05:00 -0300

    Feat: Instalação e integração do MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
-
-const routes = {
-  "/": "Curso de Express API",
-  "/livros": "Entrei na rota livros",
-  "/autores": "Entrei na rota autores",
-};
 
 app.listen(PORT, () => {
   console.log("Servidor escutando!");
