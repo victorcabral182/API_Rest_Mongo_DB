b429144d74c483dfbc7e13156340538f4b53012f
Victor Cabral <victor.cabral@globalsys.com.br>
2023-11-13 14:50:11 -0300

    Feat: Criacao do LivroController e suas rotas

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -2,8 +2,25 @@ import livro from "../models/Livro.js"
 
 class LivroController {
   static async listarLivros(req, res) {
-    const listaLivros = await livro.find({})
-    res.status(200).json(listaLivros)
+    try {
+      const listaLivros = await livro.find({})
+      res.status(200).json(listaLivros)
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na requisição` })
+    }
+  }
+
+  static async listarLivroPorId(req, res) {
+    try {
+      const livroEncontrado = await livro.findById(req.params.id)
+      res.status(200).json(livroEncontrado)
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na requisição do livro` })
+    }
   }
 
   static async cadastrarLivro(req, res) {
@@ -14,6 +31,28 @@ class LivroController {
       res
         .status(500)
         .json({ message: `${error.message} - falha ao cadastrar livro` })
+    }
+  }
+
+  static async atualizarLivro(req, res) {
+    try {
+      await livro.findByIdAndUpdate(req.params.id, req.body)
+      res.status(200).json({ message: "Livro atualizado" })
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na atualização do livro` })
+    }
+  }
+
+  static async excluirLivro(req, res) {
+    try {
+      await livro.findByIdAndDelete(req.params.id)
+      res.status(200).json({ message: "Livro excluído com sucesso" })
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: `${error.message} - falha na exclusão do livro` })
     }
   }
 }
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,6 +3,14 @@ import LivroController from "../controllers/livroController.js"
 
 const routes = express.Router()
 
+routes.post("/livros", LivroController.cadastrarLivro)
+
 routes.get("/livros", LivroController.listarLivros)
+routes.get("/livros/:id", LivroController.listarLivroPorId)
+
+routes.put("/livros/:id", LivroController.atualizarLivro)
+
+routes.delete("/livros/:id", LivroController.excluirLivro)
 
 export default routes
+s
