763f665603565994b5ca16d8a8501f69792c8302
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-11-13 23:16:19 -0300

    Finalização do curso

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -61,6 +61,18 @@ class LivroController {
         .json({ message: `${error.message} - falha na exclusão do livro` })
     }
   }
+
+  static async listarLivrosPorEditora(req, res) {
+    const editora = req.query.editora
+    try {
+      const livrosPorEditora = await livro.find({editora: editora})
+      res.status(200).json(livrosPorEditora)
+    } catch (error) {
+      res
+      .status(500)
+      .json({ message: `${error.message} - falha na busca` })
+    }
+  }
 }
 
 export default LivroController
